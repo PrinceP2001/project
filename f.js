6abbdb93f66556b8391099dc15e6f4fba939ad1f
PRINCE P <ssppprince@gmail.com>
2021-11-04 10:08:06 +0530

    home page

---
+++ f.js
@@ -0,0 +1,28 @@
+var a=10;
+let b=12;
+c=a+b;
+console.log(c);
+function add(m,n)
+{
+    x=m+n;
+    console.log("sum="+x);
+}
+add(4,8);
+let student={
+    age:24,
+    income:24000
+};
+console.log(student.age);
+if(a>b)
+{
+    console.log("true");
+}
+else{
+    console.log("false")
+}
+let j=10;
+while(j<=20)
+{
+    console.log(j);
+    j++;
+}
